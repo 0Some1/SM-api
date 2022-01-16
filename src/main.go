323d86e5991e8ec5ebd9a0d4d287f9fe83948f76
m.sadegh <mghaleyani@gmail.com>
2022-01-16 20:26:24 +0330

    [Add] server init

--- src/main.go
+++ src/main.go
@@ -9,13 +9,14 @@ import (
 )
 
 func main() {
-	fmt.Println("server run ...")
 	_, err := database.GetDatabase()
 	if err != nil {
 		fmt.Println(err)
 	}
 	router := router.Init()
-	err = http.ListenAndServe(lib.SERVER_PORT, router)
+	fmt.Println("Server started on port: " + lib.SERVER_PORT)
+	err = http.ListenAndServe(":"+lib.SERVER_PORT, router)
+
 	if err != nil {
 		fmt.Println("main - problem in run server - error: ", err)
 	}
