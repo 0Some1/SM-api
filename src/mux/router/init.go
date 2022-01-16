394cd8632ba6908639e371bf8a15f3d09e0a9096
m.sadegh <mghaleyani@gmail.com>
2022-01-16 20:28:03 +0330

    [Add] router added

--- src/mux/router/init.go
+++ src/mux/router/init.go
@@ -1,9 +1,25 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"main-api-store-management/mux/Handlers"
+	"net/http"
+)
 
 func Init() *mux.Router {
 	r := mux.NewRouter()
+
+	r.HandleFunc("/signup", Handlers.SignUp).Methods(http.MethodPost, http.MethodOptions)
+
+	r.HandleFunc("/login", Handlers.Login).Methods(http.MethodPost, http.MethodOptions)
+
+	r.HandleFunc("/store", Handlers.CreateStore).Methods(http.MethodPost, http.MethodOptions)
+
+	r.HandleFunc("/store", Handlers.GetAllStores).Methods(http.MethodGet, http.MethodOptions)
+
+	r.HandleFunc("/store/{storeID}", Handlers.DeleteStore).Methods(http.MethodDelete, http.MethodOptions)
+
+	r.NotFoundHandler = http.HandlerFunc(Handlers.NotFoundController)
 
 	return r
 }
